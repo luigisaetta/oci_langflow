7346952975b7cc95527bd83f3b986fc224899838
luigisaetta <luigi.saetta@gmail.com>
2025-01-21 17:06:52 +0100

    primo synch

---
+++ custom_oci/vectorstores/config_private.py
@@ -0,0 +1,8 @@
+"""
+config_private
+"""
+VECTOR_DB_USER = "TEST_VECTOR"
+VECTOR_DB_PWD = "GenaAiGen2024##"
+VECTOR_WALLET_DIR = ""
+VECTOR_WALLET_PWD = "welcome1"
+VECTOR_DSN = "aidb_medium"
---
+++ set_env.sh
@@ -0,0 +1,8 @@
+# sec. settings
+
+# global env
+export LANGFLOW_TRACING=false
+export LANGFLOW_COMPONENTS_PATH=/Users/lsaetta/Progetti/oci_langflow/custom_oci
+export DO_NOT_TRACK=true
+
+
